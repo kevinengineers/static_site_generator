a113d0c2d4e95c532775034b350fd3c372d5d986
kevinengineers <kevinengineers@proton.me>
2025-06-04 18:40:04 -0700

    added LeafNode class, a node in an html tree that has no children, added tests for LeafNode.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,3 +24,15 @@ class HTMLNode:
     def __repr__(self) -> str:
         return f"tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All Leaf nodes must have a value")
+
+        if self.tag == None:
+            return self.value
+
+        return f"<{self.tag}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_eq(self):
@@ -7,6 +7,17 @@ class TestHtmlNode(unittest.TestCase):
         html_node_repr = html_node.props_to_html()
         expected_value = "href=\"https://www.google.com\" "
         self.assertEqual(expected_value, html_node_repr)
+
+
+    def test_leaf_to_html(self):
+        node = LeafNode("a", "https://www.google.com")
+        self.assertEqual(node.to_html(), "<a>https://www.google.com</a>")
+
+        node2 = LeafNode("p", "This is a paragraph.")
+        self.assertEqual("<p>This is a paragraph.</p>", node2.to_html())
+
+        node3 = LeafNode("h1", "this is a level 1 heading")
+        self.assertNotEqual("<h1>This is a level 1 heading.</h1>", node3)
 
 if __name__ == "__main__":
     unittest.main()
